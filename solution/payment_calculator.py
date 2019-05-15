5f7e4bd94932bb6109a840ec37155ba535802150
wabrborich@hotmail.com <wabrborich@hotmail.com>
2019-05-15 11:11:31 -0500

    major refactor, killed shared states and made all functions pure

--- solution/payment_calculator.py
+++ solution/payment_calculator.py
@@ -1,19 +1,10 @@
 import datetime
 
-schedule1 = "00:01-09:00"
-schedule2 = "09:01-18:00"
-schedule3 = "18:01-23:59"
-weekend_bonus = 5
 
-payment_dict = {
-    schedule1: 25,
-    schedule2: 15,
-    schedule3: 20
-}
-
-with open('employee_data.txt') as f:
-    data = f.readlines()
-data = [x.strip() for x in data]
+def read_file(path):
+    with open(path) as f:
+        data = f.readlines()
+        return [x.strip() for x in data]
 
 
 def split_time_string(time_string):
@@ -22,26 +13,25 @@ def split_time_string(time_string):
             datetime.datetime.strptime(time_string[1], '%H:%M').time())
 
 
-def calculate_hour_worked_price(time_string):
+def calculate_hour_worked_price(time_string, schedule_arr, payment_dict):
     time_string = split_time_string(time_string)
-    schedule_range1 = split_time_string(schedule1)
-    schedule_range2 = split_time_string(schedule2)
-    schedule_range3 = split_time_string(schedule3)
+
+    schedule_range_arr = [split_time_string(x) for x in schedule_arr]
     init_time = time_string[0]
     end_time = time_string[1]
     hours_worked = end_time.hour - init_time.hour
     hour_price = 0
 
-    if init_time >= schedule_range1[0] and end_time <= schedule_range1[1]:
-        hour_price = float(payment_dict[schedule1])
-    elif init_time >= schedule_range2[0] and end_time <= schedule_range2[1]:
-        hour_price = float(payment_dict[schedule2])
-    elif init_time >= schedule_range3[0] and end_time <= schedule_range3[1]:
-        hour_price = float(payment_dict[schedule3])
+    for i in range(len(schedule_range_arr)):
+        if init_time >= schedule_range_arr[i][0] and end_time <= schedule_range_arr[i][1]:
+            hour_price = float(payment_dict[schedule_arr[i]])
+            break
+
     return (hour_price, hours_worked)
 
 
-def calculate_salary():
+def calculate_salary(employee_data_path, schedule_arr, payment_dict, weekend_bonus):
+    data = read_file(employee_data_path)
     for employee in data:
         separated_data = employee.split("=")
         employee_name = separated_data[0]
@@ -49,7 +39,8 @@ def calculate_salary():
         employee_salary = 0
         for payment in employe_payment_arr:
             payment_schedule = payment[2::]
-            hours_worked_price = calculate_hour_worked_price(payment_schedule)
+            hours_worked_price = calculate_hour_worked_price(payment_schedule,
+                                                             schedule_arr, payment_dict)
             if payment[0:2] in ["MO", "TU", "WE", "TH", "FR"]:
                 employee_salary += hours_worked_price[0]*hours_worked_price[1]
             else:
@@ -60,5 +51,24 @@ def calculate_salary():
             employee_name, employee_salary))
 
 
+def create_payment_dict(schedule_arr, payment_arr):
+    return dict(zip(schedule_arr, payment_arr))
+
+
 if __name__ == '__main__':
-    calculate_salary()
+
+    schedule_arr = [
+        "00:01-09:00",
+        "09:01-18:00",
+        "18:01-23:59"
+    ]
+    payment_arr = [
+        25,
+        15,
+        20
+    ]
+    weekend_bonus = 5
+    file_path = 'employee_data.txt'
+    payment_dict = create_payment_dict(
+        schedule_arr, payment_arr)
+    calculate_salary(file_path, schedule_arr, payment_dict, weekend_bonus)
--- solution/tests.py
+++ solution/tests.py
@@ -10,16 +10,56 @@ class TestCases(unittest.TestCase):
                           datetime.datetime.strptime("20:00", '%H:%M').time()))
 
     def test_salary_range1(self):
-        self.assertEqual(pc.calculate_hour_worked_price("1:00-03:00")[0], 25)
+        self.assertEqual(pc.calculate_hour_worked_price("1:00-03:00",
+                                                        schedule_arr=[
+                                                            "00:01-09:00",
+                                                            "09:01-18:00",
+                                                            "18:01-23:59"
+                                                        ],
+                                                        payment_dict={
+                                                            "00:01-09:00": 25,
+                                                            "09:01-18:00": 15,
+                                                            "18:01-23:59": 20
+                                                        })[0], 25)
 
     def test_salary_range2(self):
-        self.assertEqual(pc.calculate_hour_worked_price("10:00-12:00")[0], 15)
+        self.assertEqual(pc.calculate_hour_worked_price("10:00-12:00",
+                                                        schedule_arr=[
+                                                            "00:01-09:00",
+                                                            "09:01-18:00",
+                                                            "18:01-23:59"
+                                                        ],
+                                                        payment_dict={
+                                                            "00:01-09:00": 25,
+                                                            "09:01-18:00": 15,
+                                                            "18:01-23:59": 20
+                                                        })[0], 15)
 
     def test_salary_range3(self):
-        self.assertEqual(pc.calculate_hour_worked_price("20:00-21:00")[0], 20)
+        self.assertEqual(pc.calculate_hour_worked_price("20:00-21:00",
+                                                        schedule_arr=[
+                                                            "00:01-09:00",
+                                                            "09:01-18:00",
+                                                            "18:01-23:59"
+                                                        ],
+                                                        payment_dict={
+                                                            "00:01-09:00": 25,
+                                                            "09:01-18:00": 15,
+                                                            "18:01-23:59": 20
+                                                        })[0], 20)
 
     def test_worked_hours_calculation(self):
-        self.assertEqual(pc.calculate_hour_worked_price("10:00-12:00")[1], 2)
+        self.assertEqual(pc.calculate_hour_worked_price("10:00-12:00",
+                                                        schedule_arr=[
+                                                            "00:01-09:00",
+                                                            "09:01-18:00",
+                                                            "18:01-23:59"
+                                                        ],
+                                                        payment_dict={
+                                                            "00:01-09:00": 25,
+                                                            "09:01-18:00": 15,
+                                                            "18:01-23:59": 20
+                                                        })[1], 2)
 
 
 if __name__ == '__main__':
